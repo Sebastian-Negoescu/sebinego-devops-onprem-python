01e9fe5b58d1b55de936c98b101cc36b6ecde2aa
Negoescu, S <s.negoescu@accenture.com>
2018-11-07 17:18:52 +0200

    Testing CI/CD triggers & deployment flow.

--- deploymentScript.ps1
+++ deploymentScript.ps1
@@ -1,7 +1,7 @@
 # Azure DevOps
 # @Sebi N.
 # The template to be used when deploying the Application using the ARM Template & ARM Template Parameters Files
-# Irrelevant for now... :)
+# Irrelevant for now...! :)
 
 Login-AzureRmAccount
 
